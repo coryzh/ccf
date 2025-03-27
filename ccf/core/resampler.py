0baad76afa9e7edaec40c130d7fb79d0dc5d8410
Yue Zhao <zhao13@ualberta.ca>
2025-03-27 15:48:19 +0000

    Minor update

--- ccf/core/resampler.py
+++ ccf/core/resampler.py
@@ -2,7 +2,7 @@ import numpy as np
 from ccf.core.cross_correlation_functions import WavelengthBin
 
 
-def interp_resampler(x: np.ndarray, y: np.ndarray, 
+def interp_resampler(x: np.ndarray, y: np.ndarray,
                      bins: WavelengthBin, **kwargs) -> np.ndarray:
     y_interp = np.interp(bins.log_grid, x, y, **kwargs)
 
